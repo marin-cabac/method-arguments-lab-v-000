1673a873165f8488eeed94125d9e16420d935997
Marin Cabac <marin.cabac@gmail.com>
2018-02-07 15:20:21 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(n)
 end
 
  def introduction_with_language(n,l)
-   puts "Hi, my name is #{n} and I am learning to program in #{l}"
+   puts "Hi, my name is #{n} and I am learning to program in #{l}."
  end
